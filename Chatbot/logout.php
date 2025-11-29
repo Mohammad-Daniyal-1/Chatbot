8bef5134051e151dabb83199431d161c4759069d
Daniyal <mohddaniyal2017@gmail.com>
2025-11-29 15:35:28 +0530

    Delete Chatbot directory

--- Chatbot/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // Start the session
-session_destroy(); // Destroy the session
-header("Location: chatbot.html"); // Redirect to login page
-exit(); // Stop further script execution
-?>
